aa4a65b2d90b35bfbc3c4a4c7f243e7fd31c909e
Kim Birkelund <github.com@kimbirkelund.dk>
2012-07-04 11:32:06 +0200

    Fixing tests.

--- trunk/src/Sekhmet.Serialization.Utility.Test/Logging/LoggingAdapterBaseTest.cs
+++ trunk/src/Sekhmet.Serialization.Utility.Test/Logging/LoggingAdapterBaseTest.cs
@@ -9,49 +9,64 @@ namespace Sekhmet.Serialization.Utility.Test.Logging
     {
         public static volatile int ConstructorCalls = 0;
         public static volatile int DestructorCalls = 0;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            ConstructorCalls = 0;
-            DestructorCalls = 0;
-        }
-
+        
         [Test]
         public void TestGetLogger_GCed()
         {
+            ConstructorCalls = 0;
+            DestructorCalls = 0;
+
             var adapter = new LoggingAdapterStub();
             adapter.SetLevel("Foo.Bar.Baz", LogLevel.Info);
 
             GetAndAssertNotNull(adapter);
+            Assert.AreEqual(1, ConstructorCalls, "Expected ConstructorCalls == 1");
 
             GC.Collect();
             GC.WaitForPendingFinalizers();
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
-            Assert.AreEqual(1, ConstructorCalls);
-            Assert.AreEqual(1, DestructorCalls);
+            Assert.AreEqual(1, DestructorCalls, "Expected DestructorCalls == 1");
 
             GetAndAssertNotNull(adapter);
-            Assert.AreEqual(2, ConstructorCalls);
+
+            Assert.AreEqual(2, ConstructorCalls, "Expected ConstructorCalls == 2");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        
+            Assert.AreEqual(2, DestructorCalls, "Expected DestructorCalls == 2");
         }
 
         [Test]
         public void TestSimpleInstantiation_GCed()
         {
+            ConstructorCalls = 0;
+            DestructorCalls = 0;
+            
             CreateAndAssertNotNull();
+            Assert.AreEqual(1, ConstructorCalls, "Expected ConstructorCalls == 1");
 
             GC.Collect();
             GC.WaitForPendingFinalizers();
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
-            Assert.AreEqual(1, ConstructorCalls);
-            Assert.AreEqual(1, DestructorCalls);
+            Assert.AreEqual(1, DestructorCalls, "Expected DestructorCalls == 1");
 
             CreateAndAssertNotNull();
-            Assert.AreEqual(2, ConstructorCalls);
+
+            Assert.AreEqual(2, ConstructorCalls, "Expected ConstructorCalls == 2");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Assert.AreEqual(2, DestructorCalls, "Expected DestructorCalls == 2");
         }
 
         private static void CreateAndAssertNotNull()
